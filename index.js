ac812c84a72c3cf98fc65e251278033a5e4df2cf
user.name <akamrannicole@gmail.com>
2024-04-05 21:19:15 +0300

    functions continued lab done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(rollerskate){
+    console.log('"This Saturday, I want to roller-skate!');
+}
+// Define saturdayFun function
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define mondayWork function
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  // Define wrapAdjective function
+  function wrapAdjective(wrapper = "*") {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    }
+  }
+  
+  module.exports = { saturdayFun, mondayWork, wrapAdjective };
+  
